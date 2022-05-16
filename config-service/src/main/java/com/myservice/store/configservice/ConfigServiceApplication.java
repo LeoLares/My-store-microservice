9d7c5bbcef770efc9d1bb39117138a6c92f7a945
leolares97 <leolares97@gmail.com>
2022-05-15 23:06:29 -0300

    config server con java 8

--- config-service/src/main/java/com/myservice/store/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/myservice/store/configservice/ConfigServiceApplication.java
@@ -2,9 +2,7 @@ package com.myservice.store.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer // esta anotacion es para indicar que es un servidor de configuracion
 @SpringBootApplication
 public class ConfigServiceApplication {
 
