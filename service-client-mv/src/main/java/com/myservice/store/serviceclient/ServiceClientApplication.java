dcd2ae8f4c4daa4fe113ae59a731752ab1c91bd0
leolares97 <leolares97@gmail.com>
2022-05-23 22:44:45 -0300

    cambiamos puertoo

--- service-client-mv/src/main/java/com/myservice/store/serviceclient/ServiceClientApplication.java
+++ service-client-mv/src/main/java/com/myservice/store/serviceclient/ServiceClientApplication.java
@@ -1,5 +1,7 @@
 package com.myservice.store.serviceclient;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceClientApplication.class, args);
+		//SpringApplication.run(ServiceClientApplication.class, args);
+		SpringApplication app = new SpringApplication(ServiceClientApplication.class);
+		app.setDefaultProperties(Collections
+			.singletonMap("server.port", "8085"));
+		app.run(args);
 	}
 
 }
