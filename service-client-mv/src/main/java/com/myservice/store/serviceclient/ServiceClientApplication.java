6b27b50d7538da1e67cdfcd118d0f5a9e37d1f5f
leolares97 <leolares97@gmail.com>
2022-05-26 20:32:12 -0300

    Subismo todo

--- service-client-mv/src/main/java/com/myservice/store/serviceclient/ServiceClientApplication.java
+++ service-client-mv/src/main/java/com/myservice/store/serviceclient/ServiceClientApplication.java
@@ -4,8 +4,10 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceClientApplication {
 
 	public static void main(String[] args) {
